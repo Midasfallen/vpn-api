3e34351f793c43f17e094e7663c00dc5264a44ba
Midasfallen <84739015+Midasfallen@users.noreply.github.com>
2025-09-10 20:43:31 +0700

    ci: parse DATABASE_URL via parse_db.py and use webfactory/ssh-agent@v0.7.0

---
+++ parse_db.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import os
+import sys
+import urllib.parse as up
+
+u = os.environ.get('DBURL', '')
+if not u:
+    sys.exit(1)
+
+p = up.urlparse(u)
+print('PGHOST=' + (p.hostname or ''))
+print('PGPORT=' + (str(p.port) if p.port else ''))
+print('PGUSER=' + (p.username or ''))
+print('PGPASSWORD=' + (p.password or ''))
+print('PGDATABASE=' + (p.path[1:] if p.path else ''))
